f581fc2001fd48f9d5cf10a55c62df7171d7f784
mumu2017 <mumu2017@hotmail.com>
2015-09-14 21:57:06 +0200

    first commit

--- app/src/main/java/com/example/mumu2/broadcastbestpractice/MainActivity.java
+++ app/src/main/java/com/example/mumu2/broadcastbestpractice/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.mumu2.broadcastbestpractice;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
